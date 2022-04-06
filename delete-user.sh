63ce7939124e3061946a004e4352588bde326440
smbsid <54648899+smbsid@users.noreply.github.com>
2022-04-06 12:38:29 +0000

    Create delete-user.sh

---
+++ delete-user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Script to delete Linux users provided in a list given in argumet
+#Example: sudo bash delete-user.sh userslist.txt
+
+filename=$1
+n=1
+echo "++ Suppression en cours des users..."
+
+while read -r line;
+do
+userdel -f "$line"
+n=$((n+1))
+done < $filename
+
+echo "++ Suppression terminée"
